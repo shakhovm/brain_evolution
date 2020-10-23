12b9579a5b9af753f3bfe5f1c788fd3606dbb516
smaxym <02smaxym@gmail.com>
2020-10-23 22:45:54 +0300

    add random training and found a proper ,model

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 import gym
+from math import pi
 from time import sleep
 
 class Brain(torch.nn.Module):
@@ -12,8 +13,10 @@ class Brain(torch.nn.Module):
             self.input_size = input_size
             self.hidden_size  = hidden_size
             self.fc1 = torch.nn.Linear(self.input_size, self.hidden_size)
+            torch.nn.init.xavier_uniform(self.fc1.weight)
             self.relu = torch.nn.ReLU()
             self.fc2 = torch.nn.Linear(self.hidden_size, 1)
+            torch.nn.init.xavier_uniform(self.fc2.weight)
             self.sigmoid = torch.nn.Sigmoid()     
         
         def forward(self, x):
@@ -26,18 +29,73 @@ class Brain(torch.nn.Module):
 def classify(v):
     return 1 if v > 0.5 else 0
 
-if __name__ == '__main__':
-    brain = Brain(4,2)
+def is_alive(angle, pos):
+    return -0.5*pi < angle < 0.5*pi and abs(pos) < 2.0
+
+def demo(model):
     env = gym.make('CartPole-v0')
     env.reset()
-    for _ in range(1000):
+    data = env.step(0)[0]
+    for _ in range(10000):
         env.render()
         sleep(1/30)
         
-        val = brain.forward(torch.FloatTensor(np.array([1,2,3,1])))
+        val = model.forward(torch.FloatTensor( data ))
         val = val.detach().numpy()
         val = classify(val)
-        
-        env.step(val)
-    env.close()
     
+        data = env.step(val)[0]
+        angle = data[2]
+        pos = data[0]
+        if not is_alive(angle, pos): break
+    env.close()
+
+if __name__ == '__main__':
+    MODE = 'train'
+    
+    if MODE == 'train':
+        iters = 5000
+        half_width = 2.4
+        score = 0
+        brain = Brain(4,2)
+        best_weights = brain.state_dict()
+        best_score = 0
+
+        # brain.load_state_dict(torch.load('model_best'))
+
+        env = gym.make('CartPole-v0')
+        while iters:
+            score = 0
+            env.reset()
+            # env.render()
+            iter_data = env.step(0)[0]
+            for _ in range(1000):
+                # env.render()
+                # sleep(1/60)
+
+                val = brain.forward(torch.FloatTensor( iter_data ))
+                val = val.detach().numpy()
+                val = classify(val)
+
+                iter_data = env.step(val)[0]
+                iter_angle = iter_data[2]
+                iter_pos = iter_data[0]
+                
+                if not is_alive(iter_angle, iter_pos):
+                    if score > best_score:
+                        best_score = score
+                        best_weights = brain.state_dict()
+                    brain = Brain(4,2)
+                    print("best score: {}; {} iters left".format(best_score, iters))
+                    break
+                score += 1
+            env.close()
+            iters -= 1
+
+        print(best_score)
+        torch.save(brain.state_dict(), 'model')
+    elif MODE == 'vizualise':
+        brain = Brain(4,2)
+        brain.load_state_dict(torch.load('model'))
+        demo(brain)
+        
